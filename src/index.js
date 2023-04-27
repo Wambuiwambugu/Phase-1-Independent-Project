a78caa2df8082567bdc8d69777ea57989db539e6
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-04-27 15:48:20 +0300

    added calender css

--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ document.addEventListener("DOMContentLoaded",() => {
 
     function loadCalender(){
         const dt = new Date();
-        console.log(dt)
+        console.log(dt);
+        if (nav !== 0){
+            dt.setMonth(new Date().getMonth() + nav);
+        }
         const day = dt.getDate();
         const month = dt.getMonth();
         const year = dt.getFullYear();
-        console.log(day,month,year)
+        console.log(day,month,year);
 
         const daysInMOnth = new Date(year,month + 1, 0).getDate();
         console.log(daysInMOnth);
@@ -25,22 +28,40 @@ document.addEventListener("DOMContentLoaded",() => {
         })
         console.log(dateString)
         const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+        document.querySelector('#monthDisplay').innerText = `${dt.toLocaleDateString('en-us',{month: "long"})} ${year}`;
+
+        calender.innerHTML = '';
         for(let i = 1; i <= paddingDays + daysInMOnth; i++){
-            const daySquare = document.createElement('div')
+    
+            const daySquare = document.createElement('div');
             daySquare.classList.add('day');
             if (i > paddingDays){
                 daySquare.innerText = i - paddingDays;
-                daySquare.addEventListener('click', () => console.log(clicked))
+                daySquare.addEventListener('click', () => console.log(clicked));
             }
             else{
                 daySquare.classList.add('padding');
             }
-            calender.appendChild(daySquare)
+            calender.appendChild(daySquare);
         }
 
 
     }
-    loadCalender()
+    function initializebtns(){
+        document.querySelector('#nextbutton').addEventListener('click',() => {
+            nav++;
+            loadCalender();
+        })
+        document.querySelector('#backbutton').addEventListener('click',() => {
+            nav--;
+            loadCalender();
+        })
+    }
+    initializebtns();
+    loadCalender();
+    
+    //create services card
+    
 })
 
 
