ce1ba664f5b76a8a0daeb89890d1b44e959f5149
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-04-27 12:05:07 +0300

    addin db.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
-document.addEventListener("DOMContentLoaded",init)
+document.addEventListener("DOMContentLoaded",() => {
+    // create calender
+    let nav = 0
+    let booked = null
+    const calender = document.querySelector('#calender');
+    console.log(calender)
+    const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+    function loadCalender(){
+        const dt = new Date();
+        console.log(dt)
+        const day = dt.getDate();
+        const month = dt.getMonth();
+        const year = dt.getFullYear();
+        console.log(day,month,year)
+
+        const daysInMOnth = new Date(year,month + 1, 0).getDate();
+        console.log(daysInMOnth);
+        const firstDayOfMOnth = new Date(year,month,1)
+        const dateString = firstDayOfMOnth.toLocaleDateString('en-us',{
+            weekday: 'long',
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+        })
+        console.log(dateString)
+        const paddingDays = weekdays.indexOf(dateString.split(', ')[0])
+
+
+    }
+    loadCalender()
+})
 
 
 
-function init(){
 
-}
+
